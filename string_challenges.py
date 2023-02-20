d0bc7c183a2d788353031a5b666dcede744994e3
Stas Petrov <petrovst@mail.ru>
2023-02-20 10:32:35 +0300

    задание string_challenges

--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,46 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
+print()
 
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(len(word))
+print()
 
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
+vowels ='аоуыэеёиюя'
+vowels_list = []
+for letter in word.lower():
+    if letter in vowels:
+        vowels_list.append(letter)
+print(len(vowels_list))
+print()
 
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
-
+sentence = sentence.split()
+print(len(sentence))
+print()
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+sentence = sentence.split()
+for word in sentence:
+    print(word)
+print()    
 
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+sentence = sentence.split()
+sum = 0
+for word in sentence:
+    sum += len(word)
+average = sum//len(sentence)
+print(average)
+print()
